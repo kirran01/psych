683ff872fc745f7ae314dfd789dea1f77e69335d
kirran01 <Kirran-@live.com>
2023-03-15 19:40:55 -0400

    hmoepage updates

---
+++ src/components/wordcard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Wordcard = ({ wordCard }) => {
+    console.log(wordCard, 'wc')
+    return (
+        <div className='flex flex-col items-center text-center p-3 border-2 m-2 w-60'>
+            <p className='text-lg underline'>{wordCard.title}</p>
+            <p className='text-md'>{wordCard.description}</p>
+            <a className='text-sm underline text-cyan-600' href={`${wordCard.URL}`}>More info</a>
+        </div>
+    );
+}
+
+export default Wordcard;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import Prof from '../../public/img/prof.png'
-import pic1 from '../../public/img/k1.jpg'
-import view from '../../public/img/view.jpg'
+import view from '../../public/img/view2.jpg'
 import Footer from '../components/footer';
-import Picturecard from '../components/picturecard';
+import Wordcard from '../components/wordcard';
 import { useState } from 'react';
-import { InlineWidget } from "react-calendly";
 import { PopupWidget } from "react-calendly";
 import CBT from '../../public/img/CBT.png'
 import Maslow from '../../public/img/maslow1.png'
@@ -17,14 +14,28 @@ const Home = () => {
     { id: 2, img: Maslow, title: 'Humanistic Therapy', URL: 'https://www.bacp.co.uk/about-therapy/types-of-therapy/humanistic-therapy/#:~:text=The%20humanistic%20approach%20is%20about,with%20the%20world%20around%20you.' },
     { id: 3, img: Mindful, title: 'Mindfulness', URL: 'https://www.verywellhealth.com/mindfulness-therapy-5212796#:~:text=Summary-,Mindfulness%20therapy%20is%20a%20type%20of%20talk%20therapy%20that%20focuses,and%20to%20reduce%20automatic%20responses.' },
     { id: 4, img: Subc, title: 'Psychoanalysis', URL: 'https://tavistockandportman.nhs.uk/care-and-treatment/treatments/psychoanalytic-psychotherapy/#:~:text=Psychoanalytic%20psychotherapy%20is%20a%20talking,may%20contribute%20to%20your%20problems.' }])
+
+    const [wCards, setWCards] = useState([{ id: 1, title: 'Cognitive Behavioral Therapy', description: 'Cognitive behavioural therapy (CBT) is a talking therapy that can help you manage your problems by changing the way you think and behave.', URL: 'https://www.nhs.uk/mental-health/talking-therapies-medicine-treatments/talking-therapies-and-counselling/cognitive-behavioural-therapy-cbt/overview/#:~:text=Cognitive%20behavioural%20therapy%20(CBT)%20is,mental%20and%20physical%20health%20problems.' },
+    { id: 2, title: 'Humanistic Therapy', description: 'The humanistic approach is about free will, self-discovery and achieving your full potential as a human being, rather than concentrating on individual problems or symptoms.', URL: 'https://www.bacp.co.uk/about-therapy/types-of-therapy/humanistic-therapy/#:~:text=The%20humanistic%20approach%20is%20about,with%20the%20world%20around%20you.' },
+    { id: 3, title: 'Mindfulness', description: 'Mindfulness therapy is a type of talk therapy that focuses on learning how to be more aware of thoughts, feelings, emotions, surroundings, and situations, and to reduce automatic responses.', URL: 'https://www.verywellhealth.com/mindfulness-therapy-5212796#:~:text=Summary-,Mindfulness%20therapy%20is%20a%20type%20of%20talk%20therapy%20that%20focuses,and%20to%20reduce%20automatic%20responses.' },
+    { id: 4, title: 'Psychoanalysis', description: 'Psychoanalytic therapy helps clients gain insight into their unconscious thoughts and behaviors in order to address and resolve conflicts that may be causing emotional distress or problematic patterns of behavior.', URL: 'https://tavistockandportman.nhs.uk/care-and-treatment/treatments/psychoanalytic-psychotherapy/#:~:text=Psychoanalytic%20psychotherapy%20is%20a%20talking,may%20contribute%20to%20your%20problems.' }])
     return (
         <div className='pt-14 flex flex-col items-center fade-in'>
-            <div className='flex flex-col items-center bg-slate-50 relative'>
-                <img className='object-cover' src={view} alt="" />
-                <div className='absolute'>
-                    <p className='m-5 text-3xl text-center text-white bg-slate-900 rounded-lg p-1'>Kirran Kirpalani - General Counselor, MA</p>
-                    <p className='m-3 text-2xl text-center text-white bg-slate-800 rounded-lg p-2'>General Adolescent and Adult Counseling Across the Caribbean, ages 13+</p>
+            <div className='relative'>
+                <img className='object-cover w-full' src={view} />
+                <div className='absolute inset-0 flex flex-col items-center justify-center'>
+                    <p className='m-5 text-lg lg:text-3xl md:text-3xl text-center text-white bg-slate-800 rounded-lg p-1'>Kirran Kirpalani - General Counselor, MA</p>
+                    <p className='m-3 text-lg md:m-0 lg:m-0 mb-5 lg:text-2xl md:text-2xl text-center text-white bg-slate-800 rounded-lg p-2'>General Adolescent and Adult Counseling Across the Caribbean (ages 13+)</p>
                 </div>
+            </div>
+            <div className='flex flex-wrap justify-center p-3 py-10'>
+                {
+                    wCards.map(wc => {
+                        return (
+                            <Wordcard key={wc.id} wordCard={wc} />
+                        )
+                    })
+                }
             </div>
             <div className='flex flex-col items-center m-3'>
                 <p className='text-xl underline mb-2'>Contact Info</p>
