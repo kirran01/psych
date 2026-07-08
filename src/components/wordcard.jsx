62f543e80a13feb357f3d7222ae677bd557068ea
kirran01 <Kirran-@live.com>
2026-07-08 12:29:12 -0700

    were so back

--- src/components/wordcard.jsx
+++ src/components/wordcard.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 
 const Wordcard = ({ wordCard }) => {
     const[expand, setExpand]=useState(false)
-    console.log(wordCard, 'wc')
     return (
         <div className='flex flex-col items-center text-center p-3 border-2 m-2 w-60'>
             <p className='text-lg underline m-2'>{wordCard.title}</p>
